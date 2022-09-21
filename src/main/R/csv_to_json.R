83fd36963610b62baef6d2631bc79df784f183fe
gezever <gezever@skynet.be>
2022-09-21 13:53:55 +0200

    catalog toegevoegd en rscript refactored

--- src/main/R/csv_to_json.R
+++ src/main/R/csv_to_json.R
@@ -5,6 +5,28 @@ library(jsonlite)
 
 df <- read.csv(file = "../resources/be/vlaanderen/omgeving/data/id/conceptscheme/bioindicator/bioindicator.csv", sep=",", na.strings=c("","NA"))
 
+# hasTopConcept relatie uit inverse relatie
+schemes <- na.omit(distinct(df['topConceptOf']))
+for (scheme in as.list(schemes$topConceptOf)) {
+  topconceptof <- subset(df, topConceptOf == scheme ,
+                         select=c(uri, topConceptOf))
+  hastopconcept <- as.list(topconceptof["uri"])
+  df2 <- data.frame(scheme, hastopconcept)
+  names(df2) <- c("uri","hasTopConcept")
+  df <- bind_rows(df, df2)
+}
+
+
+
+
+
+
+
+
+
+
+
+
 tco <- subset(df, topConceptOf == 'https://data.omgeving.vlaanderen.be/id/conceptscheme/bioindicator' ,
                    select=c(uri, topConceptOf))
 htc <- as.list(tco["uri"])
@@ -22,3 +44,7 @@ df_in_list <- list('@graph' = df, '@context' = context)
 df_in_json <- toJSON(df_in_list, auto_unbox=TRUE)
 write(df_in_json, "/tmp/bioindicator.jsonld")
 
+# serialiseer jsonld naar mooie turtle en mooie jsonld
+# hiervoor dienen jena cli-tools geinstalleerd, zie README.md
+system("riot --formatted=TURTLE /tmp/bioindicator.jsonld > ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/bioindicator/bioindicator.ttl")
+system("riot --formatted=JSONLD ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/bioindicator/bioindicator.ttl > ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/bioindicator/bioindicator.jsonld")
--- src/main/bash/csv_to_rfd.sh
+++ src/main/bash/csv_to_rfd.sh
@@ -4,7 +4,3 @@
 # to jsonld, /tmp/bioindicator.jsonld
 Rscript ../R/csv_to_json.R
 
-# Make formatted jsonld and turtle
-riot --formatted=TURTLE /tmp/bioindicator.jsonld   > '../resources/be/vlaanderen/omgeving/data/id/conceptscheme/bioindicator/bioindicator.ttl'
-riot --formatted=JSONLD '../resources/be/vlaanderen/omgeving/data/id/conceptscheme/bioindicator/bioindicator.ttl'   > '../resources/be/vlaanderen/omgeving/data/id/conceptscheme/bioindicator/bioindicator.jsonld' 
-
