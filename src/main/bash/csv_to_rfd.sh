c0e68784f02f3f3c0f19387a7c5638e8de405398
gezever <gezever@skynet.be>
2024-03-25 18:56:19 +0100

    javascript

--- src/main/bash/csv_to_rfd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Transform csv, ../resources/be/vlaanderen/omgeving/data/id/conceptscheme/bioindicator/bioindicator.csv
-# to jsonld, /tmp/bioindicator.jsonld
-Rscript ../R/csv_to_json.R
-
